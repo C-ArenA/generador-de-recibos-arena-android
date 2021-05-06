12d7e046f821d795a3e0c0a5514c61b38f4beb04
C-ArenA <carlostata.ca@gmail.com>
2021-05-06 00:47:22 -0400

    Form goes into my receipt. I still have to fix the default values and make a correct validation Resolves: #5

--- app/src/main/java/com/example/recibosarena/MainActivity.kt
+++ app/src/main/java/com/example/recibosarena/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.example.recibosarena
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import com.example.recibosarena.databinding.ActivityMainBinding
+import java.lang.Error
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -11,8 +15,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setDefaultReceiptNumber()
 
-        val genericReceipt: Receipt = Receipt(binding, this)
-        Log.i("HolaMundo", genericReceipt.toString())
+        //val genericReceipt: Receipt = Receipt(binding, this)
+        //Log.i("HolaMundo", genericReceipt.toString())
+        binding.previewButton.setOnClickListener { createPreview() }
+    }
+    private fun setDefaultReceiptNumber(){
+        val sharedPreferences: SharedPreferences = getSharedPreferences("ReceiptPreferences", Context.MODE_PRIVATE)
+        val nextNumber = sharedPreferences.getInt("storedReceiptNumber", 499) + 1
+        binding.receiptNumberField.setText(String.format("%06d", nextNumber))
+    }
+
+    private fun createPreview() {
+        try {
+            val genericReceipt: Receipt = Receipt(binding, this)
+            Log.i("HolaMundo", genericReceipt.toString())
+        } catch (e: Error) {
+            Toast.makeText(this,"Rellene todos los campos con datos válidos", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+
     }
 }
--- app/src/main/java/com/example/recibosarena/Receipt.kt
+++ app/src/main/java/com/example/recibosarena/Receipt.kt
@@ -5,24 +5,33 @@ import android.content.SharedPreferences
 import android.util.Log
 import com.example.recibosarena.databinding.ActivityMainBinding
 import java.math.BigDecimal
+import java.text.DateFormat
 import java.util.*
 
 
 class Receipt(private val formBinding: ActivityMainBinding, private val context: Context) {
-    var number: String = if (formBinding.receiptNumberField.text.toString() == "") generateReceiptNumber() else formBinding.receiptNumberField.text.toString()
-    var currency: Int = BOB
+    var number: String = "000000"
+        set(stringNumber : String){
+            field = if (stringNumber == "") "000000"
+                    else String.format("%06d", stringNumber.toInt())
+        }
+    var date: Date? = Calendar.getInstance().time
+
+    var receiverName: String = "Mengano Pérez"
+    var receiverCI: String = "0000000 LP"
+    var giverName: String = "Fulano Fernández"
+    var giverCI: String = "1111111 LP"
+
     var amount: BigDecimal = BigDecimal(0)
-    var writtenAmount: String = ""
-    var date: Date = Calendar.getInstance().time
-    var transactionType: Int = CASH
-    var giverName: String = ""
-    var giverCI: String = ""
-    var receiverName: String = ""
-    var receiverCI: String = ""
-    var concept: String = ""
+    var currency: Int = BOB
+    var writtenAmount: String = "Cero Bolivianos"
+
+
+    var concept: String = "Probando la aplicación"
     var total: BigDecimal = BigDecimal(0)
     var onAccount: BigDecimal = BigDecimal(0)
-    var balance: BigDecimal = BigDecimal(0)
+    var balance: BigDecimal = total - onAccount
+    var transactionType: Int = CASH
 
     companion object {
         const val BANK = 2
@@ -32,15 +41,66 @@ class Receipt(private val formBinding: ActivityMainBinding, private val context:
         const val BOB = 0
     }
 
+    init {
+        // Number Field
+        number = formBinding.receiptNumberField.text.toString()
+        formBinding.receiptNumberField.setText(number)
+
+        // Date Field
+        val myCal = Calendar.getInstance()
+        myCal.set(formBinding.receiptDateField.year, formBinding.receiptDateField.month, formBinding.receiptDateField.dayOfMonth)
+        date = myCal.time
+
+        // Receiver and giver Fields
+        receiverName = formBinding.receiptReceiverNameField.text.toString()
+        receiverCI = formBinding.receiptReceiverCiField.text.toString()
+        giverName = formBinding.receiptGiverNameField.text.toString()
+        giverCI = formBinding.receiptGiverCiField.text.toString()
+
+        // Money
+        if (formBinding.receiptAmountField.text.toString() != ""){
+            amount = BigDecimal(formBinding.receiptAmountField.text.toString().toDouble())
+        }
+        currency = if(formBinding.receiptCurrencyTypeField.isChecked) USD else BOB
+        writtenAmount = formBinding.receiptWrittenAmountField.text.toString() + when(currency) {
+            USD -> " Dólares."
+            BOB -> " Bolivianos."
+            else -> " Bolivianos"
+        }
+
+        // More Data
+        concept = formBinding.receiptConceptField.text.toString()
+        if (formBinding.receiptTotalField.text.toString() != "") {
+            total = BigDecimal(formBinding.receiptTotalField.text.toString().toDouble())
+        }
+        if (formBinding.receiptOnAccountField.text.toString() != "") {
+            onAccount = BigDecimal(formBinding.receiptOnAccountField.text.toString().toDouble())
+        }
+
+        balance = total - onAccount
+        transactionType = when(formBinding.receiptTransactionTypeRadioGroup.checkedRadioButtonId) {
+            R.id.receipt_transaction_type_cash_radio_button -> CASH
+            R.id.receipt_transaction_type_bank_radio_button -> BANK
+            R.id.receipt_transaction_type_cheque_radio_button -> CHEQUE
+            else -> CASH
+        }
+    }
+
+
     override fun toString(): String {
         return "\n" +
                 "Número de Recibo: $number" + "\n" +
                 "Fecha: $date" + "\n" +
-                "Moneda (0->Bs, 1->Sus): $currency" + "\n" +
+                "Moneda: ${when(currency) {BOB->"Bs" 
+                    USD->"Dólares"
+                    else->"No sé"}}" + "\n" +
                 "Cantidad: $amount" + "\n" +
                 "Recibí de: $giverName" + "\n" +
                 "Con CI: $giverCI" + "\n" +
-                "Tipo de transacción: $transactionType" + "\n" +
+                "Tipo de transacción: ${when(transactionType) {CASH->"Efectivo"
+                BANK-> "Banco"
+                CHEQUE-> "Cheque"
+                else -> "No sé"}}" + "\n" +
                 "La suma de: $writtenAmount" + "\n" +
                 "Concepto: $concept" + "\n" +
                 "Total: $total" + "\n" +
@@ -50,17 +110,4 @@ class Receipt(private val formBinding: ActivityMainBinding, private val context:
                 "Con CI: $receiverCI" + "\n"
     }
 
-    private fun generateReceiptNumber(): String {
-
-        val sharedPreferences: SharedPreferences = context.getSharedPreferences("ReceiptPreferences", Context.MODE_PRIVATE)
-
-        // Next times:
-        val nextNumber = sharedPreferences.getInt("storedReceiptNumber", 499) + 1
-        with(sharedPreferences.edit()) {
-            putInt("storedReceiptNumber", nextNumber)
-            apply()
-        }
-        Log.i("SharedPreferences:", sharedPreferences.toString())
-        return String.format("%06d", nextNumber)
-    }
 }
